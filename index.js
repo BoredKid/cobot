50418f5babe7c2678966226319bc7ae915b44d67
KANE <cisse.kane@capgemini.com>
2018-04-20 11:34:51 +0200

    initialise repo

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (message) => {
+  if (message.content.startsWith('!') && message.content.endsWith('ing')) {
+    const response = `${message.content.substring(1, message.content.length - 3)}ong`;
+    message.channel.send(response);
+  }
+});
+
+client.login('NDM2ODEwNzE2NTA3NTM3NDI4.Dbs8OA.IuXt-FTRufzmte53MBYBJNvKo6E');
